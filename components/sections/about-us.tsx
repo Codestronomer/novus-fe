853c15bd63740efa23498d06f1e5e693c8e611b0
Dannyobito <dannyobito05@gmail.com>
2025-04-13 19:22:15 +0100

    responsiveness

--- components/sections/about-us.tsx
+++ components/sections/about-us.tsx
@@ -4,23 +4,29 @@ import WhaiIsNa from "@/public/Landing/what-is-na.svg";
 
 const AboutUs = () => {
   return (
-    <section className="relative px-20 pt-[5.875rem] pb-13">
-      <div className="absolute top-0 right-0 w-[319px] h-[382px]  bg-[#ADAAFC]/14 rounded-full blur-3xl pointer-events-none z-2"></div>
-      <div className="flex items-center justify-between xl:gap-[5.5rem] max-w-7xl mx-auto">
-        <div className="flex flex-col gap-5">
-          <h1 className="text-5xl font-medium tracking-tighter text-white">
+    <section className="z-10 relative px-6 sm:px-10 md:px-16 lg:px-20 pt-[5.875rem] pb-13">
+      <div className="absolute top-0 right-0 w-[200px] h-[240px] sm:w-[250px] sm:h-[300px] md:w-[319px] md:h-[382px] bg-[#ADAAFC]/14 rounded-full blur-3xl pointer-events-none z-2"></div>
+      <div className="flex flex-col lg:flex-row items-center justify-between gap-10 lg:gap-[5.5rem] max-w-7xl mx-auto">
+        <div className="flex flex-col gap-5 text-center lg:text-left">
+          <h1 className="text-3xl sm:text-4xl lg:text-5xl font-medium tracking-tighter text-white">
             <span>What is </span> <span className="text-[#F342E8]">Novus </span>
             <span>Academy?</span>
           </h1>
-          <p className="text-xl text-[#fff]/50 hover:text-white">
+          <p className="text-base sm:text-lg lg:text-xl text-[#fff]/50 hover:text-white">
             Novus Academy+ is a Web3 platform offering instructor-led, practical
             courses with on-chain certificates and crypto-friendly payments.
             Learn real skills. Prove them. Own your progress.
           </p>
         </div>
-        <Image src={WhaiIsNa} alt="" />
+        {/* <div className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-auto"> */}
+        <Image
+          className="max-w-[20rem] md:max-w-full "
+          src={WhaiIsNa}
+          alt="What is Novus Academy"
+        />
+        {/* </div> */}
       </div>
-      <div className="absolute top-0 left-[3rem] w-[300px] h-[300px]  bg-[#21FFD6]/14 rounded-full blur-[3rem] pointer-events-none z-2"></div>
+      <div className="absolute top-0 left-[1rem] sm:left-[2rem] md:left-[3rem] w-[200px] h-[200px] sm:w-[250px] sm:h-[250px] md:w-[300px] md:h-[300px] bg-[#21FFD6]/14 rounded-full blur-[2rem] sm:blur-[2.5rem] md:blur-[3rem] pointer-events-none z-2"></div>
     </section>
   );
 };
