358185d153a9bf952d7ad00e3664ac67054957e6
byte.eon <Johnrumide6@gmail.com>
2025-04-13 21:32:19 +0100

    [fix] image import"

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Link from 'next/link';
 import { Button } from '../ui/button';
+import PurpleBg from '@/public/Landing/purple.jpg';
 
 const Contact = () => {
   return (
-    <section className="flex flex-col justify-center items-center gap-4 bg-[url(@/public/landing/purple.jpg)] bg-no-repeat bg-cover p-[28px] pt-[45px]">
+    <section
+      style={{ backgroundImage: `url(${PurpleBg.src})`}}
+      className="flex flex-col justify-center items-center gap-4 bg-no-repeat bg-cover p-[28px] pt-[45px]"
+    >
       <h2 className="text-5xl text-center mt-[30px] mb-[10px] text-white font-medium tracking-tighter">Are you <span className="text-[#F342E8]">thrilled?</span></h2>
       <Link href='/waitlist'>
         <Button className="bg-[#534CFF] w-[160px] mb-[30px]" size='lg' type="submit">Join Waitlist</Button>
